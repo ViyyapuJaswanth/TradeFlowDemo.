f1457c402fb6ca29f4816f34f688ad83af3ebfbd
ViyyapuJaswanth <viyyapujaswanth1234@gmail.com>
2025-12-25 17:18:15 +0530

    Revert "Revert "Add Aerospike trade store and telemetry simulation""

---
+++ TradeFlow.Worker/TradeEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace TradeTelemetrySimulator
+{
+    public class TradeEvent
+    {
+        public string TradeId { get; set; } = Guid.NewGuid().ToString();
+        public string Symbol { get; set; } = "INFY";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime TimestampUtc { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ TradeFlow.Worker/TradeGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace TradeTelemetrySimulator
+{
+    public class TradeGenerator
+    {
+        private readonly Random _random = new();
+        private readonly string[] _symbols = new[] { "Infy", "TCS", "Wipro", "HCLTech", "LTI" };
+
+        public TradeEvent Next()
+        {
+            return new TradeEvent
+            {
+                Symbol = _symbols[_random.Next(_symbols.Length)],
+                Price = Math.Round((decimal)(_random.NextDouble() * 1000 + 100), 2),
+                Quantity = _random.Next(1, 1000), 
+                TimestampUtc = DateTime.UtcNow 
+            };
+        }
+        public IEnumerable<TradeEvent> GenerateBatch(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return Next();
+            }
+        }
+    }
+}
